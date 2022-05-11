5efbc7804208a54f7b6900d0d39c3e7fd029f537
Tosan <45423831+lodgendury@users.noreply.github.com>
2022-05-11 12:48:13 +0100

    updated domains.sol file

---
+++ contracts/Domains.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.10;
+
+import "hardhat/console.sol";
+
+contract Domains {
+  constructor() {
+    console.log("FIRST CONTRACT ON POLYGON. FUCK YEAH!");
+  }
+}
